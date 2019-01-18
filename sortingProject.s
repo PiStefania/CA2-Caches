d876e6c421ec04aec45acb0225b065219af05715
PiStefania <stefania.patsou@hotmail.com>
2019-01-18 14:04:29 +0200

    Fixed loop

--- sortingProject.s
+++ sortingProject.s
@@ -6,9 +6,18 @@ array:	.byte	1,0,3,6,2,3
 	.globl main
 main:
 		la $t0, array		# load array to temporary variable t0
-		lb $t1, 2($t0)		# get first element of array, t1 = array[0]
-		li	$v0, 1			# load appropriate system call code into register $v0, code for int is 1
-		move	$a0, $t1	# move integer to be printed into $a0:  $a0 = $t1
+		li $t1, 6			# load immediate into t1, n: number of elements in array
+		lb $t5, 0($t0)		# load first element of array to t4
+		li $t3, 0			# load index
+loop:
+		li $v0, 1			# load appropriate system call code into register $v0, code for int is 1
+		move $a0, $t5		# move integer to be printed into $a0:  $a0 = $t4
 		syscall				# call operating system to perform operation
-		li	$v0, 10			# system call code for exit = 10
+		addi $t3, $t3, 1	# add next position of array to t3
+		beq $t3, $t1, end	# go to end if index == n
+		add $t4, $t3, $t0
+		lb $t5, 0($t4)		# load value of array element
+		j loop
+end:
+		li $v0, 10			# system call code for exit = 10
 		syscall
