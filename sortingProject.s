9c13cabf71315699c32acb7db7a88e78050f15a0
Denia Athanasakou <antonia.ath@gmail.com>
2019-01-19 12:56:58 +0200

    works!!! must increase array items

--- sortingProject.s
+++ sortingProject.s
@@ -1,11 +1,11 @@
 .data
 # array of numbers -128 <= num <= 127
-array:	.byte	8, 6, 5, 3, -1, -10, 4, 20, -42, 124, 45, 53, 15, 127, 0, -128
+array:	.byte	8, 6, 5, 3, -1, -10, 4, 20, -42, 125, 45, 53, 5, 127, 0, -128, 20, -42, 8, 6
 
 	.text
 main:
 		la $t0, array		# t0 = array, load array to temporary variable t0
-		li $t1, 16			# t1 = n, load immediate into t1, n: number of elements in array
+		li $t1, 20			# t1 = n, load immediate into t1, n: number of elements in array
 		li $t3, 0			# t3 = index, load index
 gnomeLoop:
 		beq $t3, $t1, printArray	# go to end if index == n
@@ -18,7 +18,7 @@ continue:
 		#add $t5, $t5, $zero  #this is not needed
 		lb $t2, 0($t8)		# t2 = array[index-1]
 		add $t2, $t2, $zero	#without this t2 and t5 do not get any values
-		slt $t7, $t2, $t5	# t7 = result of array[index-1] > array[index] 
+		sle $t7, $t2, $t5	# t7 = result of array[index-1] > array[index] 
 		beqz $t7, swap		# if !t7, swap elements
 		addi $t3, $t3, 1	# add next position of array to t3
 		j gnomeLoop
@@ -37,7 +37,7 @@ swap:
 
 #this will be removed
 printArray: la $t0, array		# load array to temporary variable t0
-			li $t1, 16			# load immediate into t1, n: number of elements in array
+			li $t1, 20			# load immediate into t1, n: number of elements in array
 			lb $t5, 0($t0)		# load first element of array to t4
 			li $t3, 0			# load index
 loop:
@@ -49,6 +49,7 @@ loop:
 		add $t4, $t3, $t0
 		lb $t5, 0($t4)		# load value of array element
 		j loop	
+
 
 end:
 		li $v0, 10			# system call code for exit = 10
